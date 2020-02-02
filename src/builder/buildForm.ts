a8813a5c8778081fa968e6e87911e0e80863f36a
Joe Badaczewski <joe307bad@gmail.com>
2020-02-02 16:23:28 -0500

    clearing out existing html when using buildForm

--- src/builder/buildForm.ts
+++ src/builder/buildForm.ts
@@ -6,8 +6,10 @@ import { NestedCSSProperties } from 'typestyle/lib/types';
 
 export const buildForm = (entity: any) =>
     // TODO instead of sending in inputStyles, we need to send in a dictionary of input tags and NestedCSSProperties so we can apply different styles to different inputs
-    (container: string, inputStyles: NestedCSSProperties) => {
+    (containerId: string, inputStyles: NestedCSSProperties) => {
         let changes = {};
+        const container = document.getElementById(containerId)
+        container.innerHTML = '';
 
         const streams = reduce((new entity() as any), (acc: any, _value: any, key: any) => {
             const metadata = Reflect.getMetadata("metadata", new entity(), key);
@@ -17,7 +19,7 @@ export const buildForm = (entity: any) =>
             var element = document.createElement(metadata.type);
             forOwn(metadata, (value, key) => element.setAttribute(key, value))
             element.setAttribute('styles', JSON.stringify(inputStyles));
-            document.getElementById(container).appendChild(element);
+            container.appendChild(element);
 
             return merge(acc, element.stream().pipe(map(change => [key, change])))
         }, of())
