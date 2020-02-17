5e09b2bcc272ed565fc3c2af0f69d5e2784ddd36
Joe Badaczewski <joe307bad@gmail.com>
2020-02-16 21:06:37 -0500

    refining table component

--- src/components/table.ts
+++ src/components/table.ts
@@ -1,19 +1,7 @@
-import { Component, CustomElement, html } from 'ce-decorators';
+import { Component, CustomElement, html, Prop } from 'ce-decorators';
 
 import 'ag-grid-community'
 import 'ag-grid-polymer'
-
-const columnDefs = JSON.stringify([
-    { headerName: "Make", field: "make", sortable: true },
-    { headerName: "Model", field: "model" },
-    { headerName: "Price", field: "price" },
-]);
-
-const rowData = JSON.stringify([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 }
-]);
 
 @Component({
     tag: 'xura-table',
@@ -21,14 +9,21 @@ const rowData = JSON.stringify([
 })
 export class Table extends CustomElement {
 
+    @Prop({ type: Array<Object>() })
+    data: Object[] = [];
+
+    @Prop({ type: Array<Object>() })
+    columns: Object[] = [];
+
     render() {
+        debugger;
         return html`
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ag-grid/22.1.1/styles/ag-grid.min.css" />
         <link rel="stylesheet" href="https://unpkg.com/ag-grid@18.1.3-beta.1/dist/styles/ag-theme-material.css" />
         <ag-grid-polymer style="width: 100%; height: 350px;" 
             class="ag-theme-material"
-            row-data='${rowData}'
-            column-defs='${columnDefs}'
+            row-data='${JSON.stringify(this.data)}'
+            column-defs='${JSON.stringify(this.columns)}'
         >
         </ag-grid-polymer>
         `;
--- stories/table.stories.js
+++ stories/table.stories.js
@@ -4,4 +4,16 @@ export default {
     title: 'Table',
 };
 
-export const Default = () => '<xura-table></xura-table';
+const columns = JSON.stringify([
+    { headerName: "Make", field: "make", sortable: true },
+    { headerName: "Model", field: "model" },
+    { headerName: "Price", field: "price" },
+]);
+
+const data = JSON.stringify([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 }
+]);
+
+export const Default = () => `<xura-table data='${data}' columns='${columns}'></xura-table`;
